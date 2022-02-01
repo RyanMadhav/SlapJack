8d955ff7c030ca3ee193605894ee18032f454262
landy8697 <73309058+landy8697@users.noreply.github.com>
2022-02-01 07:35:27 -0600

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    document.getElementById('start-btn').addEventListener('click', () => {
+        startGame();
+    });
+    
+    
+});
+
+function startGame(){
+    alert("bruh")
+}
