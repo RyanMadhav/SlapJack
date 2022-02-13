f5207615b6dd5d5e869815a29ec2268b4f639eaa
landy8697 <landy8697@gmail.com>
2022-02-13 17:54:24 -0600

    a

--- cards.js
+++ cards.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     startGame();
-    
-    document.addEventListener('click', () => {
+    document.addEventListener('keyup', function (event) {
+        if (event.key === ' ') {
+          playerSlap();
+        }
+        
+    });
+    document.getElementById("place-button").addEventListener('mouseup', () => {
         click();
     });
     
@@ -10,56 +15,131 @@ document.addEventListener("DOMContentLoaded", function() {
 var deck;
 var hand1;
 var hand2;
+var pile;
 var loaded = false;
+var slapOpen = false;
 async function fetchJSON(URL){
     const response = await fetch(URL)
     const json = await response.json();
     return json;
 }
 var curPlayer = 0;
+var curCard;
 function getPlayer(){
     return "hand"+(curPlayer+1);
 }
+function checkWinner(){
+    fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${pile}/list/`).then(json=>{
+        console.log(json)
+        if(json['piles']['hand1']['remaining']==0){
+            alert('Your opponent won!')
+        }if(json['piles']['hand2']['remaining']==0){
+            alert('You won!')
+        }
+    })
+}
 async function click(){
+    slapOpen = false;
     console.log("click")
     fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${getPlayer()}/draw/?count=1`).then(json=>{
-        let curCard = json['cards'][0];
-        console.log(json)
+        curCard = json['cards'][0];
+        console.log(curCard)
         //if(curCard[''])
+        hand1cnt = json['piles']['hand1']['remaining'];
+        hand2cnt = json['piles']['hand2']['remaining'];
+        curLabel = document.getElementById(`p${curPlayer+1}cards`);
+        if(curPlayer == 0) curLabel.innerHTML = `Your cards: ${hand1cnt}`;
+        else curLabel.innerHTML = `Opponent cards: ${hand2cnt}`;
         curPlayer = 1 - curPlayer
+        document.getElementById('main-card').src = curCard['image']
+        fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${pile}/add/?cards=${curCard['code']}`).then(json=>{
+            //console.log(json)
+            slapOpen = true;
+            
+            handleOpponentAction();
+        })
+        
     })
+    checkWinner();
+}
+function handleSlap(player){
+    slapper = "hand"+player;
+    if(curCard['value']!='JACK'){
+        fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${slapper}/draw/?count=1`).then(json=>{
+            penalty = json['cards'][0];
+            fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${pile}/add/?cards=${penalty['code']}`).then(json=>{
+                slappercnt = json['piles'][slapper]['remaining'];
+                curLabel = document.getElementById(`p${player}cards`);
+                document.getElementById('main-card').src = penalty['image']
+                if(player == 1) curLabel.innerHTML = `Your cards: ${slappercnt}`;
+                else curLabel.innerHTML = `Opponent cards: ${slappercnt}`;
+            })
+            
+        })
+    }else{
+        fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${pile}/list/`).then(json=>{
+            console.log(json)
+            addition = getCodeList(json['piles']['pile']);
+            console.log("addition "+addition)
+            fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${slapper}/add/?cards=${addition}`).then(json=>{
+                slappercnt = json['piles'][slapper]['remaining'];
+                curLabel = document.getElementById(`p${player}cards`);
+                document.getElementById('main-card').src = "https://deckofcardsapi.com/static/img/back.png"
+                if(player == 1) curLabel.innerHTML = `Your cards: ${slappercnt}`;
+                else curLabel.innerHTML = `Opponent cards: ${slappercnt}`;
+            })
+            
+        })
+    }
+    
+}
+function getMessage(){
+    if(curCard['value']!='JACK'){
+        return '\nThe card was not a jack, 1 card placed in pile'
+    }else{
+        return '\nThe card was a jack. '
+    }
+}
+async function playerSlap(){
+    console.log('space')
+    if(!slapOpen)return;
+    slapOpen = false;
+    handleSlap(1);
+    alert('You Slapped'+getMessage());
+}
+async function opponentSlap(){
+    console.log("Can slap: "+slapOpen)
+    if(!slapOpen)return;
+    slapOpen = false;
+    handleSlap(2);
+    alert('Opponent Slapped'+getMessage());
+}
+function opponentPlaces(){
+    return Math.random() * 10 < 1;
+}
+async function handleOpponentAction(){
+    console.log("Can slap: "+slapOpen)
+    let isjack = (curCard['value']=='JACK');
+    if(isjack || opponentPlaces()){
+        timeout = 200+Math.random()*1000;
+        console.log(timeout)
+        setTimeout(() => {opponentSlap(timeout)}, timeout);
+    }
 }
 function getCodeList(json){
     let str = "";
+    console.log('hi')
+    console.log(json['cards'])
     for (let card of json['cards']){
         str += card['code']+",";
     }
     return str.substring(0, str.length-1)
 }
 function startGame(){
-    //alert("bruh")
-    //let request = new Request('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-    //console.log(request.url)
-    //let response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    /*
-    let response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-    console.log(response)
-    let json = JSON.parse(response.text)
-    console.log(json)
-    let deck = json['deck_id']
-    console.log(deck)
-    let hand1 = "hand1"
-    let hand2 = "hand2"
-    fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${hand1}/add/?cards=AS,2S`).then(json=>{
-            console.log(json)
-    })
-    fetchJSON(`https://deckofcardsapi.com/api/deck/${deck}/pile/${hand2}/add/?cards=AS,2S`).then(json=>{
-            console.log(json)
-    })
-    */
     hand1 = "hand1"
     hand2 = "hand2"
     deck = "bruh"
+    pile = "pile"
     console.log("hi")
     let got1 = false
     let got2 = false
@@ -86,42 +166,6 @@ function startGame(){
         })
         console.log("hi")
     })
-    //while(!(got1 && got2)){
-    //    continue;
-    //}
-    
-    //json = JSON.parse(response);
-    
-    
-    /*
-    fetch(request).then(function(response){
-        return response.text();
-    }).then(function(text) {
-        console.log(text)
-        deck = text.json['deck_id']
-    })
-    console.log(deck)
-    let hand1 = "hand1"
-    let hand2 = "hand2"
-    var request1 = new Request(`https://deckofcardsapi.com/api/deck/${deck}/pile/${hand1}/add/?cards=AS,2S`);
-    
-    fetch(request1)
-        .then(response => response.json())
-        .then(result=>{
-            console.log(result["piles"][hand1]["remaining"])
-        })
-    
-    
-    let request2 = new Request(`https://deckofcardsapi.com/api/deck/${deck}/pile/${hand2}/add/?count=26`);
-    */
-    /*
-    fetch(request).then(function(response) {
-        return response.text();
-    }).then(function(text) {
-        
-        
-    });
-    */
 }
 function getCardArr(){
 
